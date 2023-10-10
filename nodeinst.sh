2197cb458a5a7502580be8456cd88767d8408ef3
Leander Lombardi <leanderlombardi@gmail.com>
2023-10-10 16:50:04 +0200

    first commit

---
+++ nodeinst.sh
@@ -0,0 +1,19 @@
+echo "Please type in your password: "
+sudo su
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nodejs npm -y
+sudo npm install -g n
+sudo n stable
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "Successfully installed the latest stable Node.JS and NPM version!"
+npm -v
+node -v
